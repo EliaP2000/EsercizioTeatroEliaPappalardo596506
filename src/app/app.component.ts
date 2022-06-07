54a21ff4f45c5de65e2117916c3fa887d9f255e7
EliaP2000 <e.pappalardo1@studenti.unipi.it>
2022-06-07 15:48:48 +0200

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,12 +8,25 @@ import { ajax, AjaxResponse, AjaxRequest, AjaxError } from 'rxjs/ajax';
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
-}
+export class AppComponent   
+{   
+    title: string ="Teatro";  
+    movies: Movie[] =[  
+        {title:'Zootopia',director:'Byron Howard, Rich Moore',cast:'Idris Elba, Ginnifer Goodwin, Jason Bateman',releaseDate:'March 4, 2016'},  
+        {title:'Batman v Superman: Dawn of Justice',director:'Zack Snyder',cast:'Ben Affleck, Henry Cavill, Amy Adams',releaseDate:'March 25, 2016'},  
+        {title:'Captain America: Civil War',director:'Anthony Russo, Joe Russo',cast:'Scarlett Johansson, Elizabeth Olsen, Chris Evans',releaseDate:'May 6, 2016'},  
+        {title:'X-Men: Apocalypse',director:'Bryan Singer',cast:'Jennifer Lawrence, Olivia Munn, Oscar Isaac',releaseDate:'May 27, 2016'},  
+    ]  
+}   
+class Movie {  
+    title : string;  
+    director : string;  
+    cast : string;  
+    releaseDate : string;  
+}  
 
 //VARIABILI
-
+/*
 const chiaveTeatro = document.getElementById('key') as unknown as string;
 const nomeUtente = document.getElementById('nome') as HTMLInputElement;
 const Keyteatro = "6701ca6a";
@@ -95,4 +108,4 @@ function SecondoDiv(){
 }
 
 var plateaPrenotazione = new ordinePrenotazione(Teatro.platea, 'platea');
-var palchiPrenotazione = new ordinePrenotazione(Teatro.palchi, 'palchi');
+var palchiPrenotazione = new ordinePrenotazione(Teatro.palchi, 'palchi');*/
