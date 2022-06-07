88811de36a4418df9089d211eceee1667525f6fe
EliaP2000 <e.pappalardo1@studenti.unipi.it>
2022-06-07 11:13:47 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-bfkngg
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-bfkngg)
---
+++ src/app/app.component.ts
@@ -0,0 +1,40 @@
+import { isNull } from '@angular/compiler/src/output/output_ast';
+import { Component, VERSION } from '@angular/core';
+import { ajax, AjaxResponse, AjaxRequest, AjaxError } from 'rxjs/ajax';
+
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  name = 'Angular ' + VERSION.major;
+}
+
+const chiaveTeatro = document.getElementById('key') as unknown as string;
+const nomeUtente = document.getElementById('nome');
+const teatro = "6701ca6a";
+const div1 = document.getElementById('chiaveTeatro') as HTMLElement;
+const div2 = document.getElementById('login') as HTMLElement;
+const div3 = document.getElementById('teatro') as HTMLElement;
+
+function PrimoDiv(){
+  if(chiaveTeatro == teatro){
+    div1.style.display = 'none';
+    div2.style.display = 'show';
+  }
+  else{
+    alert("chiave errata");
+  }
+}
+
+function SecondoDiv(){
+  if(nomeUtente){
+    div3.style.display = 'show';
+  }
+  else if(nomeUtente == null){
+    alert("stringa nulla");
+    alert("inserire nome");
+  }
+}
