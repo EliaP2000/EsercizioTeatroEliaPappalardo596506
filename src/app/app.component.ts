293059f3111f883aaac5fb3848c7897d8b93e9e2
EliaP2000 <e.pappalardo1@studenti.unipi.it>
2022-06-07 12:15:47 +0200

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -39,7 +39,8 @@ class ordinePrenotazione {
   value: any;
   style: CSSStyleDeclaration;
   constructor(posti, elementName) { 
-    var element = document.getElementById(elementName);
+    var element = document.getElementById(elementName) as HTMLElement;
+    console.log(element);
     this.prenotazione = posti.map((fila, i) => { 
       var p = fila.map((nome, j) => {
         var btn = document.createElement('button'); 
