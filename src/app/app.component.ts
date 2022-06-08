6c08bede8c79a8cffb47365b8c13b7ca2e377d8b
EliaP2000 <e.pappalardo1@studenti.unipi.it>
2022-06-08 12:07:02 +0200

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -39,9 +39,6 @@ export class AppComponent
       palchi2: Array(nfilePalchi).fill("").map(() => Array(npostiPalchi).fill("P3")),
       palchi3: Array(nfilePalchi).fill("").map(() => Array(npostiPalchi).fill("P3")),
       palchi4: Array(nfilePalchi).fill("").map(() => Array(npostiPalchi).fill("P3")),
-      palchi5: Array(nfilePalchi).fill("").map(() => Array(npostiPalchi).fill("P3")),
-      palchi6: Array(nfilePalchi).fill("").map(() => Array(npostiPalchi).fill("P3")),
-      palchi7: Array(nfilePalchi).fill("").map(() => Array(npostiPalchi).fill("P3")),
     }
     ] 
 }   
@@ -59,9 +56,6 @@ class Teatro2 {
   palchi2: any[][];
   palchi3: any[][];
   palchi4: any[][];
-  palchi5: any[][];
-  palchi6: any[][];
-  palchi7: any[][];
 }  
 
 function selezionaPosto() { 
