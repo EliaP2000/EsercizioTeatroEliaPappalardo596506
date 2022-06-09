9341a79cede7bf44b105f5e33c53e40a00e151b6
EliaP2000 <e.pappalardo1@studenti.unipi.it>
2022-06-09 12:34:39 +0200

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,3 +14,54 @@ export class AppComponent {
     this.bottoni1 = Array(25).fill(0).map((x,i)=>i);
   }
 }
+/*
+function PrimoDiv(){
+  if(chiaveTeatro == Keyteatro){
+    div1.style.display = 'none';
+    div2.style.display = 'show';
+  }
+  else{
+    document.getElementById('rispostaChiave').innerHTML = 'OK';
+  }
+}
+
+function SecondoDiv(){
+  if(nomeUtente){
+    div3.style.display = 'show';
+  }
+  else if(nomeUtente == null){
+    alert("stringa nulla: inserire nome");
+  }
+}
+
+function getValue() { //creazione dell'Observable per la get
+  const obs = ajax({
+    method: 'GET',
+    url: URL + '/get?key=' + chiaveTeatro,
+    crossDomain: true,
+  });
+  obs.subscribe({
+    next: (res: AjaxResponse<any>) => {
+      document.getElementById('rispostaChiave').innerHTML = res.response; //se premuto il pulsante get da in output il valore preso nel set
+    },
+    error: (err: AjaxError) => console.error(err.response),
+  });
+}
+
+function setValue() { //creazione dell'Observable per la set
+
+  console.log(document.getElementById('data'));
+  const obs = ajax({
+    method: 'POST',
+    url:URL + '/set?key=' + chiaveTeatro,
+    crossDomain: true,
+    body: document.getElementById('key'),
+  })
+  obs.subscribe({
+    next: (res: AjaxResponse<any>) => {
+      document.getElementById('rispostaChiave').innerHTML = 'Ok!'; //response se il nuovo valore venisse settato
+    },
+    error: (err: AjaxError) => console.error(err.response),
+  });
+}*/
+
