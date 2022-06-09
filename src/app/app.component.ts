4907f2f5204ea8c922b4050b827ef44b2aa4bc65
EliaP2000 <e.pappalardo1@studenti.unipi.it>
2022-06-09 12:32:43 +0200

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent {
-  title = 'Esercizio Angular';
+  title = 'Teatro';
   bottoni = [];
   bottoni1 = [];
   constructor(){
