30007097c0452d54db657e1266f7faaa5de22ac2
EliaP2000 <e.pappalardo1@studenti.unipi.it>
2022-06-11 10:55:12 +0200

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,43 +9,11 @@ export class AppComponent {
   title = 'Teatro';
   bottoni = [];
   bottoni1 = [];
-style: any;
-value: string;
-  constructor(posti, element){
-    this.bottoni = posti.map((fila, i) => { 
-      var p = fila.map((nome, j) => {
-        var btn = document.createElement('button'); //crea i bottoni
-        element.appendChild(btn);
-        btn.value = nome;
-        btn.style.color = (nome !== "x") ? 'red' : 'green'; //cambio di colore se contenente un nome oppure una x (posto non prenotato)
-        btn.innerHTML = 'P' + (j + 1) + (i + 1); 
-        btn.addEventListener('click', this.selezionaPosto); //selezione il posto al click restituendo successivamente il valore del bottone
-        return btn;
-      });
-      element.appendChild(document.createElement('br'));
-      return p;
-    });
+  constructor(){
     this.bottoni = Array(71).fill(0).map((x,i)=>i);
     this.bottoni1 = Array(25).fill(0).map((x,i)=>i);
   }
-  selezionaPosto () { //input box per inserire un nominativo da inserire nel nostro teatro. Inserito il nominativo si fa click su un posto e il nominativo viene registrato nel bottone in corrispondenza al posto. Insieme, il colore del tasto diventa rosso per segnalare che il posto è occupato, e il contenuto dell'input box viene cancellato.
-    if ( prenotaEl.value !== "" ) {
-      this.value = prenotaEl.value;
-      this.style.color = "red";
-      prenotaEl.value="";
-    }
-    else
-      nomeEl.innerHTML = this.value 
-  };
-  toArray() { //funzione che trasforma l'array di bottoni in un array di stringhe
-    return this.prenotazione.map((fila) =>
-      fila.map( x => x.value)
-    );
-  }
-}
-}
-const prenotaEl = document.getElementById('prenota');
-const nomeEl = document.getElementById('nome');
+
 /*
 function PrimoDiv(){
   if(chiaveTeatro == Keyteatro){
@@ -96,4 +64,5 @@ function setValue() { //creazione dell'Observable per la set
     error: (err: AjaxError) => console.error(err.response),
   });
 }*/
+
 
