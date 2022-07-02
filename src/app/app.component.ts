6866f0922a02d2593aac1a62c6025a8b9f7bd183
EliaP2000 <e.pappalardo1@studenti.unipi.it>
2022-07-02 16:59:29 +0200

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,8 +16,7 @@ export class AppComponent {
     this.bottoni = Array(71).fill(0).map((x,i)=>i);
     this.bottoni1 = Array(25).fill(0).map((x,i)=>i);
     for(var i=0; i<this.bottoni.length; i++){
-      var valoreBottone = this.bottoni[i];
-      console.log(this.bottoni[i])
+      var valoreBottone = document.getElementById('bottoni');
       valoreBottone.addEventListener('click', this.selezionaPosto, false);
     }
     for(var i=0; i<this.bottoni1.length; i++){
