ff120af5b5df9b8a38ac4d06ace317671ca4f037
EliaP2000 <e.pappalardo1@studenti.unipi.it>
2022-07-02 16:17:36 +0200

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -33,54 +33,15 @@ toArray() { //funzione che trasforma l'array di bottoni in un array di stringhe
     );
   };
 }
+var comments = document.getElementsByClassName('button');
+var numComments = comments.length;
 
-/*
-function PrimoDiv(){
-  if(chiaveTeatro == Keyteatro){
-    div1.style.display = 'none';
-    div2.style.display = 'show';
-  }
-  else{
-    document.getElementById('rispostaChiave').innerHTML = 'OK';
-  }
+function showComment() {
+  var place = document.getElementById('textfield');
+  var commentBox = document.createElement('textarea');
+  place.appendChild(commentBox);
 }
 
-function SecondoDiv(){
-  if(nomeUtente){
-    div3.style.display = 'show';
-  }
-  else if(nomeUtente == null){
-    alert("stringa nulla: inserire nome");
-  }
+for (var i = 0; i < numComments; i++) {
+  comments[i].addEventListener('click', showComment, false);
 }
-
-function getValue() { //creazione dell'Observable per la get
-  const obs = ajax({
-    method: 'GET',
-    url: URL + '/get?key=' + chiaveTeatro,
-    crossDomain: true,
-  });
-  obs.subscribe({
-    next: (res: AjaxResponse<any>) => {
-      document.getElementById('rispostaChiave').innerHTML = res.response; //se premuto il pulsante get da in output il valore preso nel set
-    },
-    error: (err: AjaxError) => console.error(err.response),
-  });
-}
-
-function setValue() { //creazione dell'Observable per la set
-
-  console.log(document.getElementById('data'));
-  const obs = ajax({
-    method: 'POST',
-    url:URL + '/set?key=' + chiaveTeatro,
-    crossDomain: true,
-    body: document.getElementById('key'),
-  })
-  obs.subscribe({
-    next: (res: AjaxResponse<any>) => {
-      document.getElementById('rispostaChiave').innerHTML = 'Ok!'; //response se il nuovo valore venisse settato
-    },
-    error: (err: AjaxError) => console.error(err.response),
-  });
-}*/
