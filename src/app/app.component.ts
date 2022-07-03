cfcf5811fc90d6dcd3bf2e45ebc8fce0c5716366
EliaP2000 <e.pappalardo1@studenti.unipi.it>
2022-07-03 16:19:25 +0200

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
 const prenotazione = document.getElementById('prenota') as HTMLInputElement;
+console.log(prenotazione)
 const nome = document.getElementById('nome') as HTMLInputElement;
+console.log(nome)
 const ClasseBottoni = document.getElementsByClassName('bottoni');
+console.log(ClasseBottoni)
 
 @Component({
   selector: 'my-app',
@@ -16,6 +19,8 @@ export class AppComponent {
   constructor(){
     this.bottoni = Array(71).fill(0).map((x,i)=>i);
     this.bottoni1 = Array(25).fill(0).map((x,i)=>i);
+    console.log(this.bottoni)
+    console.log(this.bottoni1)
     for(var i=0; i<this.bottoni1.length; i++){
       var valoreBottone1 = this.bottoni1[i];
       valoreBottone1.addEventListener('click', this.selezionaPosto(), false);
