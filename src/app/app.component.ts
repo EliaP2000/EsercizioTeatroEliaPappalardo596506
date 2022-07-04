46809bcf56831f720bcaeed26a029e24019d669c
EliaP2000 <e.pappalardo1@studenti.unipi.it>
2022-07-04 12:09:57 +0200

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -26,8 +26,8 @@ export class AppComponent {
     document.addEventListener("click", this.selezionaPosto, true);
   };
 selezionaPosto(){
-  var length = this.bottoni.length;
-    for(var i=0; i<length; i++){
+  var lunghezzaArray = this.bottoni.length;
+    for(var i=0; i<lunghezzaArray; i++){
       if(this.bottoni[i] == this.bottoni[i].addEventListener("click", true)){
         this.bottoni[i].value = info;
         this.bottoni[i].style.color = "red";
