47d344005a15e12f5a3662017156a1f6893f4b1b
EliaP2000 <e.pappalardo1@studenti.unipi.it>
2022-07-04 10:50:36 +0200

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,7 +24,7 @@ export class AppComponent {
     console.log(this.bottoni)
     console.log(this.bottoni1)
     var length = this.bottoni.length;
-    prenotazione.addEventListener("click", prenotazione.sceltaPosto(),{
+    prenotazione.addEventListener("click", prenotazione.sceltaPosto(){
       for(let i=0; i<length; i++){
         this.bottoni[i].addEventListener("click", this.bottoni[i].selezionaPosto, false);
       }
