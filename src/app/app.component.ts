372a0ac7dfc2f6efba8a65b75924b46b78192942
EliaP2000 <e.pappalardo1@studenti.unipi.it>
2022-07-04 10:37:41 +0200

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-const prenotazione = (document.getElementById("prenota") as HTMLInputElement).value;
+const prenotazione = document.getElementById("prenota").value;
 console.log(prenotazione)
 let info = document.getElementById("data") as unknown as string;
 console.log(info)
