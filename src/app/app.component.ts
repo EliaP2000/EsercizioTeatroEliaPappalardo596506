ef0a19e9c76a83ce594f3efa80fa112f3771b63f
EliaP2000 <e.pappalardo1@studenti.unipi.it>
2022-07-05 16:48:02 +0200

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-window.onload(){
-  let Inserimento:HTMLElement = document.getElementsByClassName("divInserimento")[0] as HTMLElement;
-  let Teatro:HTMLElement = document.getElementsByClassName("divTeatro")[0] as HTMLElement;
-  let info = document.getElementById("data") as unknown as string;
-  const nome = document.getElementById("nome") as HTMLInputElement;
-  const ClasseBottoni = document.getElementsByClassName("bottoni");
-}
+
+
+let Inserimento:HTMLElement = document.getElementsByClassName("divInserimento")[0] as HTMLElement;
+let Teatro:HTMLElement = document.getElementsByClassName("divTeatro")[0] as HTMLElement;
+let info = document.getElementById("data") as unknown as string;
+const nome = document.getElementById("nome") as HTMLInputElement;
+const ClasseBottoni = document.getElementsByClassName("bottoni");
 
 
 @Component({
