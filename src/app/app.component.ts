aeb055f1bbb3bacefdf6216817dbba5c6c9a05e5
EliaP2000 <e.pappalardo1@studenti.unipi.it>
2022-07-06 11:03:58 +0200

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 
-
 let Teatro:HTMLElement = document.getElementsByClassName("divTeatro")[0] as HTMLElement;
 let info = document.getElementById("data") as unknown as string;
 const nome = document.getElementById("nome") as HTMLInputElement;
