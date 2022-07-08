7a10eef3e891d8651619d56f49d4542024988008
EliaP2000 <e.pappalardo1@studenti.unipi.it>
2022-07-08 17:11:37 +0200

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,35 +7,39 @@ console.log(info)*/
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   public value: any;
   public style: CSSStyleDeclaration;
   bottoni = [];
   bottoni1 = [];
-  constructor(){
-    this.bottoni = Array(70).fill(0).map((x,i)=>i+1);
-    this.bottoni1 = Array(24).fill(0).map((x,i)=>i+1);
-  };
-public selezionaPostoPlatea(info: string, Numplatea: any){
-  for(var j=0; j<this.bottoni.length; j++){
-      if(Numplatea==j){
+  constructor() {
+    this.bottoni = Array(70)
+      .fill(0)
+      .map((x, i) => i + 1);
+    this.bottoni1 = Array(24)
+      .fill(0)
+      .map((x, i) => i + 1);
+  }
+  public selezionaPostoPlatea(info: string, Numplatea: any) {
+    for (var j = 0; j < this.bottoni.length; j++) {
+      if (Numplatea == j) {
         this.bottoni[j] = info;
-      //this.bottoni[j].style.backgroundColor = "red";
-        info="";
+        //this.bottoni[j].style.backgroundColor = "red";
+        info = '';
       }
     }
-  };
-public selezionaPostoPalchi(info: string, Numpalchi: any){
-  for(var j=0; j<this.bottoni1.length; j++){
-    if(Numpalchi==j){
-      this.bottoni1[j] = info;
-      //this.bottoni1[j].style.backgroundColor = "red";
-      info="";
+  }
+  public selezionaPostoPalchi(info: string, Numpalchi: any) {
+    for (var j = 0; j < this.bottoni1.length; j++) {
+      if (Numpalchi == j) {
+        this.bottoni1[j] = info;
+        //this.bottoni1[j].style.backgroundColor = "red";
+        info = '';
       }
     }
-  };
+  }
 }
 
 var plateaPrenotazione = new AppComponent();
@@ -45,25 +49,31 @@ var Numpalchi: any;
 var info: string;
 //var nomeInserito: any;
 
-function showTeatro(nomeInserito: any){
-  var BloccoInserimento = Array.from(document.getElementsByClassName("divInserimento") as unknown as HTMLCollectionOf<HTMLElement>);
-  var BloccoTeatro = Array.from(document.getElementsByClassName("divTeatro") as unknown as HTMLCollectionOf<HTMLElement>);
-    if(nomeInserito != null){
-      for(var j=0; j<BloccoInserimento.length; j++){
-        BloccoInserimento[j].style.visibility='hidden';
-        BloccoTeatro[j].style.visibility="visibile";
-        return BloccoInserimento||BloccoTeatro; 
-      }
+function showTeatro(nomeInserito: any) {
+  var BloccoInserimento = Array.from(
+    document.getElementsByClassName(
+      'divInserimento'
+    ) as unknown as HTMLCollectionOf<HTMLElement>
+  );
+  var BloccoTeatro = Array.from(
+    document.getElementsByClassName(
+      'divTeatro'
+    ) as unknown as HTMLCollectionOf<HTMLElement>
+  );
+  if (nomeInserito != null) {
+    for (var j = 0; j < BloccoInserimento.length; j++) {
+      BloccoInserimento[j].style.visibility = 'hidden';
+      BloccoTeatro[j].style.visibility = 'visibile';
+      return BloccoInserimento || BloccoTeatro;
     }
-    else{
-      for(var j=0; j<BloccoInserimento.length; j++){
-        BloccoInserimento[j].style.visibility='visible';
-        BloccoTeatro[j].style.visibility="hidden";
-        return BloccoInserimento||BloccoTeatro; 
-      }
+  } else {
+    for (var j = 0; j < BloccoInserimento.length; j++) {
+      BloccoInserimento[j].style.visibility = 'visible';
+      BloccoTeatro[j].style.visibility = 'hidden';
+      return BloccoInserimento || BloccoTeatro;
     }
+  }
 }
 
 plateaPrenotazione.selezionaPostoPlatea(info, Numplatea);
 palchiPrenotazione.selezionaPostoPalchi(info, Numpalchi);
-document.getElementById("data").addEventListener("click", showTeatro);
